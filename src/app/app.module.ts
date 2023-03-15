31988d34a99d808441970b90ad8fcbb280b32018
Ghofrane <gbenrejeb2000@gmail.com>
2023-03-15 11:55:55 +0100

    Page playlist proposé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+import { PlaylistSuggereComponent } from './playlist-suggere/playlist-suggere.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    PlaylistSuggereComponent
   ],
   imports: [
     BrowserModule
